0ee018f5d01df6e06503e95218bbc669082f1547
Justin Lee Edwards <justin@highdefdesigns.com>
2023-03-24 00:39:34 -0500

    crud finalized

---
+++ routes/ideas.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const router = express.Router();
+
+const ideas = [
+  {
+    id: 1,
+    text: 'Positive Newsletter, a newsletter that only shares positive, uplifting news',
+    tag: 'Technology',
+    username: 'TonyStark',
+    date: '2022-01-02',
+  },
+  {
+    id: 2,
+    text: 'Milk cartons that turn a different color the older thtat your milk is getting',
+    tag: 'Inventions',
+    username: 'SteveRogers',
+    date: '2022-01-02',
+  },
+  {
+    id: 3,
+    text: 'ATM location ap which lets you know where the closest ATM is and if it is in service',
+    tag: 'Software',
+    username: 'BruceBannr',
+    date: '2022-01-02',
+  },
+];
+
+// get all ideas
+router.get('/', (req, res) => {
+  res.json({ success: true, data: ideas });
+});
+
+// get single idea
+router.get('/:id', (req, res) => {
+  const idea = ideas.find((idea) => idea.id === +req.params.id);
+
+  if (!idea) {
+    return res
+      .status(404)
+      .json({ success: false, error: 'Resource not found' });
+  }
+  res.json({ success: true, data: idea });
+});
+
+// POST
+router.post('/', (req, res) => {
+  const idea = {
+    id: ideas.length + 1,
+    text: req.body.text,
+    tag: req.body.tag,
+    username: req.body.username,
+    date: new Date().toISOString().slice(0, 10),
+  };
+
+  ideas.push(idea);
+
+  res.json({ success: true, data: idea });
+});
+// UPDATE
+router.put('/:id', (req, res) => {
+  const idea = ideas.find((idea) => idea.id === +req.params.id);
+
+  if (!idea) {
+    return res
+      .status(404)
+      .json({ success: false, error: 'Resource not found' });
+  }
+  idea.text = req.body.text || idea.text;
+  idea.tag = req.body.tag || idea.tag;
+
+  res.json({ success: true, data: idea });
+});
+// DELETE
+router.delete('/:id', (req, res) => {
+  const idea = ideas.find((idea) => idea.id === +req.params.id);
+
+  if (!idea) {
+    return res
+      .status(404)
+      .json({ success: false, error: 'Resource not found' });
+  }
+
+  const index = ideas.indexOf(idea);
+  ideas.splice(index, 1);
+
+  res.json({ success: true, data: {} });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,52 +3,16 @@ const port = 5501;
 
 const app = express();
 
-const ideas = [
-  {
-    id: 1,
-    text: 'Positive Newsletter, a newsletter that only shares positive, uplifting news',
-    tag: 'Technology',
-    username: 'TonyStark',
-    date: '2022-01-02',
-  },
-  {
-    id: 2,
-    text: 'Milk cartons that turn a different color the older thtat your milk is getting',
-    tag: 'Inventions',
-    username: 'SteveRogers',
-    date: '2022-01-02',
-  },
-  {
-    id: 3,
-    text: 'ATM location ap which lets you know where the closest ATM is and if it is in service',
-    tag: 'Software',
-    username: 'BruceBannr',
-    date: '2022-01-02',
-  },
-];
+// Use this for POSTing (body parser middleware)
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-// Body parser middleware
-
+// GET
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to the RandomIdeas API' });
 });
 
-// get all ideas
-app.get('/api/ideas', (req, res) => {
-  res.json({ success: true, data: ideas });
-});
-
-// get single idea
-app.get('/api/ideas/:id', (req, res) => {
-  const idea = ideas.find((idea) => idea.id === +req.params.id);
-
-  if (!idea) {
-    return res
-      .status(404)
-      .json({ success: false, error: 'Resource not found' });
-  }
-
-  res.json({ success: true, data: idea });
-});
+const ideasRouter = require('./routes/ideas');
+app.use('/api/ideas', ideasRouter);
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
